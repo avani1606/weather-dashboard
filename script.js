0ba0c5826b0444fcea66538f6a56c86af9461c9c
Avani Goel <avanigoel2006@gmail.com>
2026-07-31 22:30:02 +0530

    Add fetch API call to get weather data

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const searchBtn = document.getElementById("searchBtn");
-
-searchBtn.addEventListener("click", function() {
-  const city = document.getElementById("cityInput").value;
-  console.log(city);
+const API_KEY="a3c8262719edbbe9b777badf6b485548";
+const searchBtn=document.getElementById("searchBtn");
+searchBtn.addEventListener("click", async function(){
+  const city=document.getElementById("cityInput").value;
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  const response=await fetch(url);
+  const data=await response.json();
+  console.log(data);
 });
