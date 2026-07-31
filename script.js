b14698833a1ec2fa7ca50c3c4f0a76fe9dc14304
Avani Goel <avanigoel2006@gmail.com>
2026-07-31 22:45:36 +0530

    Display Weather data on page via DOM manipulation

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const API_KEY="a3c8262719edbbe9b777badf6b485548";
 const searchBtn=document.getElementById("searchBtn");
 searchBtn.addEventListener("click", async function(){
+  console.log("button clicked");
   const city=document.getElementById("cityInput").value;
   const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   const response=await fetch(url);
   const data=await response.json();
   console.log(data);
+  document.getElementById("temperature").textContent = data.main.temp + "°C";
+document.getElementById("cityName").textContent = data.name;
+document.getElementById("feelsLike").textContent = data.main.feelsLike + "°C";
+document.getElementById("condition").textContent = data.weather[0].description;
+document.getElementById("humidity").textContent = data.main.humidity + "%";
+document.getElementById("windSpeed").textContent = data.wind.speed + "mph";
 });
